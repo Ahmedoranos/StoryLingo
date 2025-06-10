868f5e392bdebfc8e66187a41217b14d360896f6
Ahmed <ahmedcairo95@hotmail.com>
2025-06-10 06:17:10 +0300

    questions added

--- src/app/component/beneficiarycomponent/lesson-content/lesson-content-b1/lesson-content-b1-1/lesson-content-b1-1.component.ts
+++ src/app/component/beneficiarycomponent/lesson-content/lesson-content-b1/lesson-content-b1-1/lesson-content-b1-1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { VerbConjugation } from 'src/app/models/verb-conjugation';
+import { FlashCard } from 'src/app/models/flash-card';
 
 @Component({
   selector: 'app-lesson-content-b1-1',
@@ -18,4 +19,46 @@ verbConjugations: VerbConjugation[] = [
     verbName: "erhalten"
   }
 ];
+
+
+flashcards: (FlashCard | null)[][] = [
+  [
+    { german: 'der Beleg', english: 'the receipt/document', isFlipped: false },
+    null,
+    null,
+    { german: 'die Belege', english: 'the receipts/documents', isFlipped: false }
+  ],
+  [
+    { german: 'der Auftrag', english: 'the order/assignment', isFlipped: false },
+    null,
+    null,
+    { german: 'die Aufträge', english: 'the orders/assignments', isFlipped: false }
+  ],
+  [
+    { german: 'der Ausgang', english: 'the exit/outcome', isFlipped: false },
+    null,
+    null,
+    { german: 'die Ausgänge', english: 'the exits/outcomes', isFlipped: false }
+  ],
+  [
+    null,
+    { german: 'die Angst', english: 'the fear/anxiety', isFlipped: false },
+    null,
+    { german: 'die Ängste', english: 'the fears/anxieties', isFlipped: false }
+  ]
+];
+
+
+qTellFriend1: string = 'Tell your friend in German he should not be afraid of dogs; they are friendly.';
+correctSentence1: string = "Du brauchst keine Angst vor Hunden zu haben, sie sind freundlich.";
+
+
+qcanGiveReceipt2: string = 'Can you give me a receipt/proof?';
+correctSentence2: string = "Kannst du mir einen Beleg geben?";
+
+
+qTranslate3: string = 'Translate the following sentence to german "We gave them a receipt"';
+correctSentence3: string = "Wir haben ihnen einen Beleg gegeben.";
+
+
 }
